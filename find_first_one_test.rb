5e2eee682aa642a5ad6a58b137ea671f32f690c0
Nathaniel Watts <reg@nathanielwatts.com>
2013-09-17 15:39:31 -0600

    add test_fifth_or_second_to_last_sparkly_unicorn to find_first_one_test.rb

--- find_first_one_test.rb
+++ find_first_one_test.rb
@@ -46,5 +46,30 @@ class FindFirstOneTest < Minitest::Test
 
     assert_equal unicorn4.object_id, found.object_id
   end
+
+  def test_fifth_or_second_to_last_sparkly_unicorn
+    skip
+    unicorn1  = Unicorn.new('white')
+    unicorn2  = Unicorn.new('sparkly')
+    unicorn3  = Unicorn.new('purple')
+    unicorn4  = Unicorn.new('pink')
+    unicorn5  = Unicorn.new('sparkly')
+    unicorn6  = Unicorn.new('white')
+    unicorn7  = Unicorn.new('purple')
+    unicorn8  = Unicorn.new('pink')
+    unicorn9  = Unicorn.new('sparkly')
+    unicorn10 = Unicorn.new('pink')
+
+    unicorns_a = [unicorn1, unicorn2, unicorn3, unicorn4, unicorn5, 
+                  unicorn6, unicorn7, unicorn8, unicorn9, unicorn10]
+    
+    unicorns_b = [unicorn1, unicorn2, unicorn3, unicorn4, 
+                  unicorn6, unicorn7, unicorn8, unicorn9, unicorn10]
+
+    # write code here
+
+    assert_equal unicorn5.object_id, found_a.object_id
+    assert_equal unicorn9.object_id, found_b.object_id
+  end
 end
 
