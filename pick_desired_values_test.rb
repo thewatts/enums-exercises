ba4bb8c4731e1be0dd3597d10c4142bd1f1c80d3
Nathaniel Watts <reg@nathanielwatts.com>
2013-09-18 13:30:27 -0600

    finish pick desired values test... cause apparently I forgot

--- pick_desired_values_test.rb
+++ pick_desired_values_test.rb
@@ -7,43 +7,58 @@ class PickDesiredValuesTest < Minitest::Test
     numbers = (1..10)
     evens = []
     numbers.each do |number|
-      # write code here
+      evens << number if number.to_i.even?
     end
     assert_equal [2, 4, 6, 8, 10], evens
   end
 
   def test_pick_odd_numbers
-    skip
+    #skip
     numbers = (1..10)
-    # write code here
+    odds = []
+    numbers.each do |num|
+      odds << num if num.odd?
+    end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
+    #skip
     words = %w(pill bad finger cat blue dog table red)
-    # write code here
+    selected = []
+    words.each do |w|
+      selected << w if w.length == 3
+    end
     assert_equal %w(bad cat dog red), selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
+    #skip
     words = %w(pill bad finger cat blue dog table red)
-    # write code here
+    selected = []
+    words.each do |w|
+      selected << w if w.length > 3
+    end
     assert_equal %w(pill finger blue table), selected
   end
 
   def test_pick_words_ending_in_e
-    skip
+    #skip
     words = %w(are you strike thinking belt piece warble sing pipe)
-    # write code here
+    selected = []
+    words.each do |w|
+      selected << w if w.end_with? "e"
+    end
     assert_equal %w(are strike piece warble pipe), selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
+    #skip
     words = %w(drought singing hat shoehorning purposeful)
-    # write code here
+    selected = []
+    words.each do |w|
+      selected << w if w.end_with? "ing"
+    end
     assert_equal %w(singing shoehorning), selected
   end
 end
